f0ce3ce49aeb9b26e0dcfa584afce61df0265cd0
unknown <shrutinambiar@SHRUTI3794.Nagarro.local>
2015-12-17 12:10:57 +0530

    PMARA-1020<PMARA-1078> : removed : logging was done two times in prepareparameter

--- tools/src/main/java/com/automic/nexus/actions/UploadArtifactAction.java
+++ tools/src/main/java/com/automic/nexus/actions/UploadArtifactAction.java
@@ -74,9 +74,7 @@ public class UploadArtifactAction extends AbstractHttpAction {
             extension = getOptionValue("extension");
 
             if (!CommonUtil.checkNotEmpty(packaging) && !CommonUtil.checkNotEmpty(extension)) {
-                String msg = "At least Package or Extension should be provided ";
-                LOGGER.error(msg);
-                throw new AutomicException(msg);
+                throw new AutomicException( "At least Package or Extension should be provided ");
             }
         } catch (AutomicException e) {
             LOGGER.error(e);
